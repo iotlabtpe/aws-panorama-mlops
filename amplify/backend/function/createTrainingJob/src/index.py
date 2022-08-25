f1923144414519a4703bbb4078e3c338bcdb05e2
You Kai <kaiyour@amazon.com>
2022-08-25 10:34:38 +0800

    fix typo in createTrainingJob

--- amplify/backend/function/createTrainingJob/src/index.py
+++ amplify/backend/function/createTrainingJob/src/index.py
@@ -60,7 +60,7 @@ def handler(event, context):
             labels_prefix = event["labels_prefix"]
 
         # Create a role for training job to perform action successfully
-        role_arn = boto3.client("ssm").get_parameter(Name="/ppe/config/arn")["Parameter"]["Value"]
+        role_arn = boto3.client("ssm").get_parameter(Name="/ppe/config/arn/" + os.environ["ENV"])["Parameter"]["Value"]
         instance_type = "ml.p3.2xlarge"
         if "instance_type" in event:
             instance_type = event["instance_type"]
